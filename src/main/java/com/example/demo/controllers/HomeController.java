2a4aca312f782934b29b54a133e93d7ac49f16ba
Anastasiya Piashko <anapiashko@gmail.com>
2020-05-17 23:01:12 +0300

    create template

--- src/main/java/com/example/demo/controllers/HomeController.java
+++ src/main/java/com/example/demo/controllers/HomeController.java
@@ -2,15 +2,14 @@ package com.example.demo.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @Controller
 public class HomeController {
 
-    @RequestMapping("/")
-    @ResponseBody
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     String home() {
-        return "Hello World!";
+        return "hello";
     }
 }
