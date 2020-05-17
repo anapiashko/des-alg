57b8833040d7642003cc2800a061c9a91e4ec0fc
Anastasiya Piashko <anapiashko@gmail.com>
2020-05-18 02:57:53 +0300

    create endpoints

--- src/main/java/com/example/demo/controllers/HomeController.java
+++ src/main/java/com/example/demo/controllers/HomeController.java
@@ -1,14 +1,28 @@
 package com.example.demo.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
 
     @GetMapping(value = "/")
     public String home() {
+        return "start";
+    }
 
+    @PostMapping("/encryption")
+    public String encrypt(@RequestParam String sourceText, Model model) {
+        model.addAttribute("text", sourceText+"SHIFT");
+        return "start";
+    }
+
+    @PostMapping("/decryption")
+    public String decrypt(@RequestParam String sourceText, Model model) {
+        model.addAttribute("text", sourceText+"SHIFT1");
         return "start";
     }
 }
