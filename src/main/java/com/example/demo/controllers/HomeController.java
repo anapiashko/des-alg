031cdcc70eb5023397fbe8212a7347c2e26b28ca
Anastasiya Piashko <anapiashko@gmail.com>
2020-05-17 23:02:56 +0300

    delete

--- src/main/java/com/example/demo/controllers/HomeController.java
+++ src/main/java/com/example/demo/controllers/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @RequestMapping(value = "/")
     String home() {
         return "hello";
     }
